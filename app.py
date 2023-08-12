f7b94e2ee1febda95547d3c512bb9b074bea146c
thuannv <thuanthienhuong@gmail.com>
2023-08-12 23:57:56 +0700

    implement code to detect license plate.

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ app = Flask(__name__)
 def root():
     return render_template('index.html')
 
+@app.route("/detect-license-plate", methods =["GET"])
+def page_detect_license_plate():
+    return render_template('detect_license_plate.html')
+
+def root():
+    return render_template('index.html')
+
 
 @app.route("/detect", methods=["POST"])
 def detect():
@@ -22,6 +29,30 @@ def detect():
       json.dumps(boxes),
       mimetype='application/json'
     )
+
+@app.route("/detect-license-plate", methods=["POST"])
+def detect_license_plate():
+    buf = request.files["image_file"]
+    boxes = handle_detect_license_plate(Image.open(buf.stream))
+    return Response(
+      json.dumps(boxes),
+      mimetype='application/json'
+    )
+
+def handle_detect_license_plate(buf):
+    model = YOLO("best_license_plate.pt")
+    results = model.predict(buf)
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [
+            round(x) for x in box.xyxy[0].tolist()
+        ]
+        prob = round(box.conf[0].item(), 2)
+        output.append([
+            x1, y1, x2, y2, result.names[0], prob
+        ])
+    return output
 
 
 def detect_objects_on_image(buf, type):
---
+++ static/javascripts/detect_license.js
@@ -0,0 +1,37 @@
+
+const input = document.getElementById("uploadInput");
+input.addEventListener("change", async (event) => {
+    handle_data(event.target.files[0]);
+})
+
+async function handle_data(file)
+{
+    const data = new FormData();
+    data.append("image_file", file, "image_file");
+    document.getElementById("title_image").innerText = "Handling..."
+    const response = await fetch("/detect-license-plate", {
+        method: "post",
+        body: data
+    });
+    const boxes = await response.json();
+    draw_image_and_boxes(file, boxes);
+}
+
+function draw_image_and_boxes(file, boxes) {
+    const img = new Image()
+    img.src = URL.createObjectURL(file);
+    img.onload = () => {
+        document.getElementById("title_image").innerText = "Please check result: Detect "+ boxes.length + " result."
+        const canvas = document.querySelector("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+        ctx.strokeStyle = "#00FF00";
+        ctx.lineWidth = 3;
+        ctx.font = "18px serif";
+        boxes.forEach(([x1, y1, x2, y2, label]) => {
+            ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+        });
+    }
+}
